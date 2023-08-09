28774ae0ef93c2c39d793a2fea933f10c72faa58
regtyit <titov54275@gmail.com>
2023-08-09 18:17:11 +0400

    keywords component

--- src/components/KeyWords.vue
+++ src/components/KeyWords.vue
@@ -1,8 +1,33 @@
 <script setup>
+import content from "../db/1301100102.js"
 </script>
 <template>
-    <v-card-text>
-     ключевые слова
-  
-    </v-card-text>
+        <v-card-text>
+    <span>Ключевые слова</span>
+<v-table>
+    <tbody>
+      <tr
+        v-for="item in content.keywords"
+        :key="item.word"
+      >
+        <td class="word">{{ item.word }}</td>
+        <td class="description">{{ item.description }}</td>
+      </tr>
+    </tbody>
+</v-table>
+</v-card-text>
 </template>
+<style scoped>
+.word {
+    width: 35%;
+}
+.description{
+    width: 65%;
+}
+td {
+  border: 1px solid ;
+}
+span{
+    font-size:xx-large;
+}
+</style>
