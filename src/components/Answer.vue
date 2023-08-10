5c9b82f193b9dabdb1c2b8cbdc65e82e3229ba9f
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-10 19:02:00 +0400

    added answer component

---
+++ src/components/Answer.vue
@@ -0,0 +1,22 @@
+<script setup>
+let props = defineProps({
+  answer: {
+    type: Object,
+    default: null
+  },
+  entered: {
+    type: Boolean,
+  }
+})
+</script>
+
+<template>
+  <div 
+    :class="`d-flex flex-row align-stretch rounded-lg ${props.entered ? (props.answer.right ? 'bg-green-lighten-4' : 'bg-red-lighten-4') : props.answer.selected ? 'bg-blue-lighten-5' : 'bg-white'}`"
+    :style="`cursor: pointer; user-select: none; ${props.answer.selected ? 'box-shadow: 0px 0px 0px 1px #000000;' : ''}`"
+  >
+    <div class="pa-4">
+      {{ props.answer.text }}
+    </div>
+  </div>
+</template>
