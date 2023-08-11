2023e4ef0bc220926d6ecee4b105c2cb7a5a5518
Katya-com <katerina.yad.24@gmail.com>
2023-08-11 17:58:44 +0400

    questions1

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -15,8 +15,7 @@ let props = defineProps({
     :class="`d-flex flex-row align-stretch rounded-lg ${props.entered ? (props.answer.right ? 'bg-green-lighten-4' : 'bg-red-lighten-4') : props.answer.selected ? 'bg-blue-lighten-5' : 'bg-white'}`"
     :style="`cursor: pointer; user-select: none; ${props.answer.selected ? 'box-shadow: 0px 0px 0px 1px #000000;' : ''}`"
   >
-    <div class="pa-4">
-      {{ props.answer.text }}
+    <div class="pa-4" v-html="props.answer.text">
     </div>
   </div>
 </template>
