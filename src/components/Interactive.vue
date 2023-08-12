a4d55b21312030c2e2959250ca51689c57aebb0e
qbclub <17926057+qbclub@users.noreply.github.com>
2023-08-12 10:00:05 +0400

    add slides

--- src/components/Interactive.vue
+++ src/components/Interactive.vue
@@ -4,33 +4,49 @@ import { useDB } from '../stores/db'
 
 let code = useRoute().query.code
 const content = await useDB().getFromCode(code)
+
+// let imgUrl = (index) => { return new URL(content.interactive.slides[index], import.meta.url) }
 </script>
 
 <template>
-    <div v-if="content.interactive.type == 'video'"  class="container-video">
+    <div v-if="content.interactive.type == 'video'" class="container-video">
         <h2>{{ content.theme }}</h2>
         <video width="320" controls autoplay>
             <source :src="content.interactive.video" type="video/mp4">
         </video>
     </div>
-    <div v-if="content.interactive.type == 'slide-show'">
-        Тут слайд-шоу
-    </div>
+    <v-row v-if="content.interactive.type == 'slides'" class="justify-center align-center " style="height:90dvh">
+        <h2>{{ content.theme }}</h2>
+        <v-col cols="12" md="8">
+
+            <v-carousel hide-delimiters>
+                <v-carousel-item v-for="slide, index in content.interactive.slides" :key="index" :src="slide">
+    
+                </v-carousel-item>
+
+
+            </v-carousel>
+        </v-col>
+
+    </v-row>
 </template>
 <style scoped>
-.container-video{
-    display:flex;
+.container-video {
+    display: flex;
     height: 100dvh;
     width: 100%;
     justify-content: center;
     align-items: center;
     flex-direction: column;
 }
-video, h2{
+
+video,
+h2 {
     width: 60%;
     text-align: center;
 }
-h2{
+
+h2 {
     font-size: clamp(1rem, 0.5294rem + 0.9412vw, 1.5rem);
 }
 </style>
