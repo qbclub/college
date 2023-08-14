f7fe019006e538219e719ed791f6df98b0db7e45
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-14 15:10:59 +0400

    fixed answer

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -12,19 +12,8 @@ let props = defineProps({
 
 <template>
   <div
-    :class="`d-flex answer rounded-lg ${props.entered ? (props.answer.right ? 'bg-green-lighten-4' : 'bg-red-lighten-4') : props.answer.selected ? 'bg-blue-lighten-5' : 'bg-white'}`"
-    :style="`cursor: pointer; user-select: none; ${props.answer.selected ? 'box-shadow: 0px 0px 0px 1px #000000;' : ''}`">
-    <div class="pa-4" v-html="props.answer.text">
-    </div>
-  </div>
+    :class="`pa-4 text-center rounded-lg ${props.entered ? (props.answer.right ? 'bg-green-lighten-4' : 'bg-red-lighten-4') : props.answer.selected ? 'bg-blue-lighten-5' : 'bg-white'}`"
+    :style="`cursor: pointer; user-select: none; word-wrap: break-word; white-space: break-spaces; ${props.answer.selected ? 'box-shadow: 0px 0px 0px 1px #000000;' : ''}`"
+    v-html="props.answer.text"
+  />
 </template>
-<style scoped>
-.answer {
-  box-sizing: border-box;
-  min-height: 80px;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-
-}
-</style>
