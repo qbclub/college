87b775c7a86609b82715cedc0e18ab1771b4013f
qbclub <17926057+qbclub@users.noreply.github.com>
2023-08-17 11:10:53 +0400

    220105010101

--- src/stores/db.js
+++ src/stores/db.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useDB = defineStore('db', () => {
   async function getFromCode(code) {
-		return await (await fetch(`/db/${code || 130110010102}.json`)).json()
+		return await (await fetch(`/db/${code || 220105010101}.json`)).json()
 	}
 
   return { getFromCode }
