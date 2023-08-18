8fe650412bfd80fe70621e8f9c7f5e85a862806b
SaveliyShutov <savelijsutov@gmail.com>
2023-08-18 14:25:56 +0400

    220205020201

--- src/stores/db.js
+++ src/stores/db.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useDB = defineStore('db', () => {
   async function getFromCode(code) {
-		return await (await fetch(`/db/${code || '150216050501_2' }.json`)).json()
+		return await (await fetch(`/db/${code || 220205020201 }.json`)).json()
 	}
 
   return { getFromCode }
