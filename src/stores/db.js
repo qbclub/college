797cf962c57b5bded3f10259fb4443acb8773f22
SaveliyShutov <savelijsutov@gmail.com>
2023-08-18 16:27:54 +0400

    150132010101 key words

--- src/stores/db.js
+++ src/stores/db.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useDB = defineStore('db', () => {
   async function getFromCode(code) {
-		return await (await fetch(`/db/${code || '150132010101_2' }.json`)).json()
+		return await (await fetch(`/db/${code || '150132010101' }.json`)).json()
 	}
 
   return { getFromCode }
