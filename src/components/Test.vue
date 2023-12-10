6409ca9fea41c6a3ec7bc94f7d4d27b07708cfa7
qbclub <17926057+qbclub@users.noreply.github.com>
2023-12-10 18:48:19 +0400

    sort test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router';
 import { useDB } from '../stores/db';
 import Answer from './Answer.vue';
 import AnswerInput from './AnswerInput.vue';
+import Sort from './Sort.vue';
 import Result from './Result.vue';
 
 let code = useRoute().query.code
@@ -14,7 +15,7 @@ let test = reactive(content.test)
 // Добавляем тех переменные
 for (let question of test.questions) {
 	question.entered = false
-	
+
 	if (question.type === 'input') {
 		for (let answer of question.answers) {
 			answer.value = ''
@@ -28,7 +29,7 @@ for (let question of test.questions) {
 }
 
 let current_step = ref(1)
-let current_question = computed(() => test.questions[current_step.value-1])
+let current_question = computed(() => test.questions[current_step.value - 1])
 
 let steps = computed(() => test.questions.length)
 let done = computed(() => current_step.value > steps.value)
@@ -40,7 +41,7 @@ let entered_amount = ref(0)
 /********** Methods **********/
 
 function answerQuestion() {
-	test.questions[current_step.value-1].entered = true
+	test.questions[current_step.value - 1].entered = true
 }
 
 function increaseStep() {
@@ -56,17 +57,17 @@ function back() {
 function select(index, value = null) {
 	// Если radio и что-то введено, значит очищаем что введено
 	if (current_question.value.type === 'once' && current_question.value.answers.some(item => item.selected) && !current_question.value.entered)
-		test.questions[current_step.value-1].answers.forEach(answer => answer.selected = false)
+		test.questions[current_step.value - 1].answers.forEach(answer => answer.selected = false)
 
 	if (current_question.value.entered)
 		return
 
-	let answer = test.questions[current_step.value-1].answers[index]
+	let answer = test.questions[current_step.value - 1].answers[index]
 	answer.selected = !answer.selected
 }
 
 function compareInput(input, right) {
-  return (new RegExp(right)).test(input.trim().toLowerCase())
+	return (new RegExp(right)).test(input.trim().toLowerCase())
 }
 
 // Обновляем количество введенных и правильно введенных
@@ -96,36 +97,31 @@ let seconds = 0
 let minutes = 0
 
 function updateTime() {
-  seconds++;
-  if (seconds === 60) {
-    minutes++;
-    seconds = 0;
-  }
-  timer.value = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+	seconds++;
+	if (seconds === 60) {
+		minutes++;
+		seconds = 0;
+	}
+	timer.value = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 let interval = setInterval(updateTime, 1000)
 
 watch(done, (value) => {
-  if (value)
-	  clearInterval(interval)
+	if (value)
+		clearInterval(interval)
 })
 </script>
 
 <template>
-  <div class="container">
+	<div class="container">
 		<div class="w-100 d-flex flex-column">
 			<div class="w-100 font-weight-light pt-4 pr-8 pb-4 pl-8 rounded-lg text-center "
 				style="color:black; background: white;">
 				{{ content.theme }}
 			</div>
-	
-			<div 
-				v-if="!done" 
-				class="w-100 pa-4 d-flex flex-row align-center"
-			>
-				<div 
-					class="ml-3 text-h6"
-				>
+
+			<div v-if="!done" class="w-100 pa-4 d-flex flex-row align-center">
+				<div class="ml-3 text-h6">
 					{{ current_step }}. {{ current_question.question }}
 				</div>
 			</div>
@@ -136,36 +132,24 @@ watch(done, (value) => {
 		</div>
 
 		<v-fade-transition leave-absolute hide-on-leave>
-			<div 
-				v-if="!done"
-				class="h-100 mt-4 pb-4" 
-				style="width: 85%;"	
-			>
-				<v-row 
-					class="flex-column"
-					style="margin: auto;"
-				>
+			<div v-if="!done" class="h-100 mt-4 pb-4" style="width: 85%;">
+				<v-row class="flex-column" style="margin: auto;">
 					<v-fade-transition group>
-						<v-col
-							v-for="(answer, index) in current_question.answers"
-							:key="index"
-						>
-							<AnswerInput
-								v-if="current_question.type === 'input'"
-								:answer="answer"
-								:entered="current_question.entered"
-							>
+						<v-col v-for="(answer, index) in current_question.answers" :key="index">
+							<AnswerInput v-if="current_question.type === 'input'" :answer="answer"
+								:entered="current_question.entered">
 								<template v-slot:input>
-									<input :class="{border: true, 'bg-green-lighten-3': current_question.entered && compareInput(answer.value, answer.answer), 'bg-red-lighten-3': current_question.entered && !compareInput(answer.value, answer.answer)}" type="text" v-model="test.questions[current_step-1].answers[index].value" :disabled="current_question.entered">
+									<input
+										:class="{ border: true, 'bg-green-lighten-3': current_question.entered && compareInput(answer.value, answer.answer), 'bg-red-lighten-3': current_question.entered && !compareInput(answer.value, answer.answer) }"
+										type="text" v-model="test.questions[current_step - 1].answers[index].value"
+										:disabled="current_question.entered">
 								</template>
 							</AnswerInput>
 
-							<Answer 
-								v-else
-								@click="select(index)"
-                :answer="answer"
-								:entered="current_question.entered"
-							/>
+							<Sort v-if="current_question.type === 'sort'" :answer="answer"  :entered="current_question.entered"
+								/>
+
+							<Answer v-if="current_question.type === 'once'" @click="select(index)" :answer="answer" :entered="current_question.entered" />
 						</v-col>
 					</v-fade-transition>
 				</v-row>
@@ -173,49 +157,28 @@ watch(done, (value) => {
 		</v-fade-transition>
 
 		<v-fade-transition leave-absolute hide-on-leave>
-			<Result 
-				v-if="done"
-				:right="right_entered_amount"
-				:answersAmount="entered_amount"
-				:time="timer"
-			/>
+			<Result v-if="done" :right="right_entered_amount" :answersAmount="entered_amount" :time="timer" />
 		</v-fade-transition>
 
 		<v-spacer />
 
-		<div 
-			class="w-100 pa-4 d-flex flex-row justify-center align-center flex-wrap" 
-			style="background: #456DB1; position: sticky; bottom: 8px; column-gap: 30px; row-gap: 8px;"
-		>
-			<div 
-				class="text-white"
-				style="font-size: 20px; font-weight: 500;"
-			>
+		<div class="w-100 pa-4 d-flex flex-row justify-center align-center flex-wrap"
+			style="background: #456DB1; position: sticky; bottom: 8px; column-gap: 30px; row-gap: 8px;">
+			<div class="text-white" style="font-size: 20px; font-weight: 500;">
 				{{ timer }}
 			</div>
 
-			<v-btn
-				v-if="current_step > 1"
-				@click="back()"
-				:ripple="false"
-			>
+			<v-btn v-if="current_step > 1" @click="back()" :ripple="false">
 				На шаг назад
 			</v-btn>
 
-			<v-btn 
-				v-if="!done && !current_question.entered"
-				@click="answerQuestion()"
+			<v-btn v-if="!done && !current_question.entered" @click="answerQuestion()"
 				:disabled="current_question.type == 'input' ? current_question.answers.every(answer => !answer.value.length) : current_question.answers.every(item => !item.selected)"
-				:ripple="false"
-			>
+				:ripple="false">
 				Ответить
 			</v-btn>
 
-			<v-btn
-				v-if="current_question && current_question.entered"
-				@click="increaseStep()"
-				:ripple="false"
-			>
+			<v-btn v-if="current_question && current_question.entered" @click="increaseStep()" :ripple="false">
 				Дальше
 			</v-btn>
 		</div>
@@ -225,7 +188,7 @@ watch(done, (value) => {
 <style lang="scss" scoped>
 .container {
 	background-attachment: fixed;
-	background-color: rgba(241,242,242,.5);
+	background-color: rgba(241, 242, 242, .5);
 	// background-image: linear-gradient(#fafafa 0,#fafafa 130px,rgba(68,28,80,.1) 160px,#fff 100%);
 	// box-shadow: 0 0 0.11rem 0 rgb(0 0 0 / 30%);
 
